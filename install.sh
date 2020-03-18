7474190b6d38a3f7d3f91849ddc841a1a1a206ae
qushot <moto@yoshidumi.co.jp>
2020-03-18 21:36:40 +0900

    hgoe

--- install.sh
+++ install.sh
@@ -1,23 +1,26 @@
 #!/bin/zsh
 
-# https://qiita.com/kinchiki/items/57e9391128d07819c321
-# この記事のマネをしてインストールする
+DOTFILES_DIR="$HOME/.ditfiles"
+
+git clone https://github.com/qushot/dotfiles.git $DOTFILES_DIR
+cd $DOTFILES_DIR
 
 git submodule add -f https://github.com/sorin-ionescu/prezto.git .zprezto
 git submodule update --init --recursive
 
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/dotfiles/.zprezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR:-$DOTFILES_DIR}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-ln -sf ~/dotfiles/.zprezto ~/.zprezto
-ln -sf ~/dotfiles/prompt_qushot_setup ~/.zprezto/modules/prompt/functions/prompt_qushot_setup
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+ln -sf $DOTFILES_DIR/.zprezto ~/.zprezto
+ln -sf $DOTFILES_DIR/prompt_qushot_setup ~/.zprezto/modules/prompt/functions/prompt_qushot_setup
+ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
+ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
+ln -sf $DOTFILES_DIR/.zpreztorc ~/.zpreztorc
 
 source ~/.zpreztorc
 source ~/.zshrc
 
 # rm -rf .zlogin .zlogout .zprofile .zpreztorc .zshenv .zshrc .zprezto
+# rm -rf .git .gitmodules .zprezto
